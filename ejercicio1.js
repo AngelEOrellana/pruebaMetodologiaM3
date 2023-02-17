7446d01acee2dbc51aa58ee34493969a97e95bca
Angelorellanaroman <>
2023-02-17 07:28:25 -0500

    ingreso datos

---
+++ ejercicio1.js
@@ -0,0 +1,32 @@
+
+let url= prompt("Ingresa la url:");
+//https://drive.google.com/file/d/1Jn3S96hTm0goTFKaBvXh6BJ6B_gpUGRt/view?usp=share_link
+
+let res="";
+let posIni=0;
+let posFin=0;
+
+for(let i=0;i<url.length;i++){
+
+    if(url.substring(i,i+2)=== "d/"){
+        posIni=i+2;
+    }
+
+    if(url.substring(i,i+2)=== "/v"){
+        posFin=i;
+    }
+}
+
+res = url.substring(posIni,posFin);
+console.log("Resultado del string: " + res);
+
+let doc = document.createElement("img");
+let cadena = `https://drive.google.com/uc?export=view&id=` + res;
+
+//doc.src=`https://drive.google.com/uc?export=view&id=${res}`;
+console.log("Resultado del string: " + res + "url completa: " + cadena);
+
+doc.src=cadena;
+let d = document.getElementById("ruta");
+d.appendChild(doc);
+
